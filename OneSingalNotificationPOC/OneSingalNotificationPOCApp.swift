96ea3daa9d76bd94faf01a6a455ee0d53e542361
GuruMahan <iosdeve2022@gmail.com>
2023-02-16 13:26:24 +0530

    Initial Commit

---
+++ OneSingalNotificationPOC/OneSingalNotificationPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneSingalNotificationPOCApp.swift
+//  OneSingalNotificationPOC
+//
+//  Created by Guru Mahan on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct OneSingalNotificationPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
